2306c4a8f32d91f10c7edc262bd695be91b0e611
danielsobrado <daniel@danielsobrado.com>
2022-04-16 14:15:34 +0400

    add description

--- leetcode/add-two-numbers/src/add_two_sum.rs
+++ leetcode/add-two-numbers/src/add_two_sum.rs
@@ -15,6 +15,11 @@ impl ListNode {
   }
 }
 
+// You are given two non-empty linked lists representing two non-negative integers. 
+// The digits are stored in reverse order, and each of their nodes contains a single digit. 
+// Add the two numbers and return the sum as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
 impl Solution {
     pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         
