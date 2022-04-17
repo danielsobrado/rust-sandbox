f098df861945fe3fc2377f8ed55aca0accd5789a
danielsobrado <daniel@danielsobrado.com>
2022-04-17 18:58:27 +0400

    Example of using  functional reduce and fold

--- leetcode/longest-common-prefix/src/longest_common_prefix.rs
+++ leetcode/longest-common-prefix/src/longest_common_prefix.rs
@@ -4,7 +4,32 @@ struct Solution;
 // If there is no common prefix, return an empty string "".
 
 impl Solution {
+
+    pub fn longest_str(strs: Vec<String>) -> String {
+        let str = strs.iter().fold(strs[0], |acc, &item| {
+            if str.len() > acc.len() {
+                str
+            } else {
+                acc
+            }
+        });
+    
+        return Some(str);
+    }
+
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
+
+        let str = longest_str(strs).unwrap_or("");
+
+        let result: Vec<char> = Vec::with_capacity(str.len());
+
+        for (i, c) in str.chars().enumerate() {
+            let val: bool = strs.iter().chars.collect()
+                .inspect(|x| println!("about to filter: {}", x))
+                .filter(|&x| x[i] == c)
+                .inspect(|x| println!("made it through filter: {}", x))
+                .fold(false, |false, i| sum + i);
+        }
         String::from("")
     }
 }
