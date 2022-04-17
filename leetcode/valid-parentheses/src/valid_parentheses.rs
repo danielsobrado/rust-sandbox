5cdea8da5e8636434aee8e509bb4d894d7534119
danielsobrado <daniel@danielsobrado.com>
2022-04-17 12:43:17 +0400

    Leetcode: Valid Parenthesis progress

--- leetcode/valid-parentheses/src/valid_parentheses.rs
+++ leetcode/valid-parentheses/src/valid_parentheses.rs
@@ -1,7 +1,37 @@
 struct Solution;
 
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+
 impl Solution {
     pub fn is_valid(s: String) -> bool {
+
+        if (s.len() % 2) != 0 {
+            return false;
+        }
+
+        if s.is_empty() {
+            return true;
+        }
+
+        let mut stack: Vec<char> = Vec::new();
+
+        for c in s.chars() {
+            match c {
+                '(' => stack.push(')'),
+                '{' => stack.push('}'),
+                '[' => stack.push(']'),
+                _ => {
+                    if stack.pop() != Some(c) {
+                        return false;
+                    }
+                }
+            }
+        }
+
         true
     }
 }
@@ -19,4 +49,14 @@ fn test2() {
 #[test]
 fn test3() {
     assert_eq!(Solution::is_valid(String::from("(]")), false);
+}
+
+#[test]
+fn test4() {
+    assert_eq!(Solution::is_valid(String::from("((")), false);
+}
+
+#[test]
+fn test5() {
+    assert_eq!(Solution::is_valid(String::from("((")), false);
 }
