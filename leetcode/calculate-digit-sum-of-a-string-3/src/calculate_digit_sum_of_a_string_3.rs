01c2bebf34503bada0a51f54fbd39c85ff83ee4a
danielsobrado <daniel@danielsobrado.com>
2022-04-17 08:11:29 +0400

    Leetcode: Solution for calculate_digit_sum_of_a_string_3

--- leetcode/calculate-digit-sum-of-a-string-3/src/calculate_digit_sum_of_a_string_3.rs
+++ leetcode/calculate-digit-sum-of-a-string-3/src/calculate_digit_sum_of_a_string_3.rs
@@ -11,7 +11,52 @@ struct Solution;
 // Return s after all rounds have been completed.
 impl Solution {
     pub fn digit_sum(s: String, k: i32) -> String {
-        String::from("")
+
+        if s.len() <= k as usize {
+            return s;	
+        }
+
+        
+        // Divide in groups of k
+        let mut groups = vec![];
+        let mut group = String::new();
+        for c in s.chars() {
+            group.push(c);
+            if group.len() == k as usize {
+                groups.push(group);
+                group = String::new();
+            }
+        }
+        if !group.is_empty() {
+            groups.push(group);
+        }
+        
+        // Replace each group with the sum of its digits
+        let mut groups2 = vec![String::from(""); groups.len()];   // Create a new vector with the same length as groups to avoid borrow checker issues
+        for (i, group) in groups.iter_mut().enumerate() {
+            let mut sum = 0;
+            for c in group.chars() {
+                sum += c.to_digit(10).unwrap();
+            }
+            println!("Group {} - {}: {} ", i, group, sum);
+            groups2[i] = sum.to_string();
+        }
+    
+        // Merge groups
+        let mut result = String::new();
+        for group in groups2 {
+            println!("Merging {}", group);
+            result.push_str(&group);
+        }
+        
+        // Repeat until no more groups
+        if result.len() > k as usize {
+            println!("Iterate over {}", result);
+            Solution::digit_sum(result, k)
+        } else {
+            result
+        }
+
     }
 }
 
@@ -23,4 +68,14 @@ fn test1() {
 #[test]
 fn test2() {
     assert_eq!(Solution::digit_sum(String::from("00000000"), 3), String::from("000"));
+}
+
+#[test]
+fn test3() {
+    assert_eq!(Solution::digit_sum(String::from("233"), 3), String::from("233"));
+}
+
+#[test]
+fn test4() {
+    assert_eq!(Solution::digit_sum(String::from("11111222223"), 3), String::from("135"));
 }
