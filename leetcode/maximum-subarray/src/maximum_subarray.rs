82599220d7ab126d59c163ddc803dffc9f8a091d
danielsobrado <daniel@danielsobrado.com>
2022-04-19 07:33:38 +0400

    Leetcode: Solution max_sub_array

--- leetcode/maximum-subarray/src/maximum_subarray.rs
+++ leetcode/maximum-subarray/src/maximum_subarray.rs
@@ -6,7 +6,19 @@ struct Solution;
 
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        0
+
+        if nums.is_empty() {
+            return 0;
+        }
+
+        let mut max_sum = nums[0];
+        let mut current_sum = nums[0];
+        
+        for i in 1..nums.len() {
+            current_sum = i32::max(current_sum + nums[i], nums[i]);
+            max_sum = i32::max(max_sum, current_sum);
+        }
+        max_sum
     }
 }
 
