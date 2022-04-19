a8093e40f008557eecb6b5c5475a31f8a65ac0ed
danielsobrado <daniel@danielsobrado.com>
2022-04-19 11:02:26 +0400

    Leetcode: Solution for contains_duplicate

--- leetcode/contains-duplicate/src/contains_duplicate.rs
+++ leetcode/contains-duplicate/src/contains_duplicate.rs
@@ -3,8 +3,25 @@ struct Solution;
 // Problem URL: https://leetcode.com/problems/contains-duplicate/
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+use std::collections::HashSet;
+
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+
+        if nums.is_empty() {
+            return false;
+        }
+
+        let mut nums_set = HashSet::new();
+
+        for num in nums {
+            if nums_set.contains(&num) {
+                return true;
+            } else {
+                nums_set.insert(num);
+            }
+        }
+
         false
     }
 }
