45d6d00e2f3b3e8c0843eaa27f4ba39d9e310f24
danielsobrado <daniel@danielsobrado.com>
2022-04-19 11:14:59 +0400

    correct test cases for merge-sorted-array

--- leetcode/merge-sorted-array/src/merge_sorted_array.rs
+++ leetcode/merge-sorted-array/src/merge_sorted_array.rs
@@ -12,21 +12,27 @@ struct Solution;
 
 impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        0
+     
     }
 }
 
 #[test]
 fn test1() {
-    assert_eq!(Solution::merge([1,2,3,0,0,0], 3, [2,5,6], 3), [1,2,2,3,5,6]), [1,2,2,3,5,6]);
+    let mut nums1 = vec![1,2,3,0,0,0];
+    Solution::merge(&mut nums1, 3, &mut vec![2,5,6], 3);
+    assert_eq!(nums1, [1,2,2,3,5,6]);
 }
 
 #[test]
 fn test2() {
-    assert_eq!(Solution::merge([1], 1, [], 0), [1]); ;
+    let mut nums1 = vec![1];
+    Solution::merge(&mut nums1, 1, &mut vec![], 0);
+    assert_eq!(nums1, [1]);
 }
 
 #[test]
 fn test3() {
-    assert_eq!(Solution::merge([0], 0, [1], 1), [1]);
+    let mut nums1 = vec![0];
+    Solution::merge(&mut nums1, 0, &mut vec![1], 1);
+    assert_eq!(nums1, [1]);
 }
