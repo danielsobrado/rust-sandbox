82594f3d1d13a07c05994b3f5bb798e6160830a4
danielsobrado <daniel@danielsobrado.com>
2022-04-22 07:55:39 +0400

    Leetcode: Solution for squares_of_a_sorted_array

--- leetcode/squares-of-a-sorted-array/src/squares_of_a_sorted_array.rs
+++ leetcode/squares-of-a-sorted-array/src/squares_of_a_sorted_array.rs
@@ -4,25 +4,35 @@ struct Solution;
 // Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
 impl Solution {
-    pub fn squares_of_a_sorted_array(nums: &mut Vec<i32>) {
+    pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
 
-        if nums.len() <= 1 {
-            return;
+        if nums.len() == 0 {
+            return nums;
         }
+
+        let mut result = nums.iter().map(|x| x * x).collect::<Vec<i32>>();
+
+        result.sort_unstable();
+
+        result
     }
 
 }
 
 #[test]
 fn test1() {
-    let mut nums = vec![-4,-1,0,3,10];
-    Solution::squares_of_a_sorted_array(&mut nums);
-    assert_eq!(nums, [0,1,9,16,100]);
+    let nums = vec![-4,-1,0,3,10];
+    assert_eq!(Solution::sorted_squares(nums), [0,1,9,16,100]);
 }
 
 #[test]
 fn test2() {
-    let mut nums = vec![-7,-3,2,3,11];
-    Solution::squares_of_a_sorted_array(&mut nums);
-    assert_eq!(nums, [4,9,9,49,121]);
+    let nums = vec![-7,-3,2,3,11];
+    assert_eq!(Solution::sorted_squares(nums), [4,9,9,49,121]);
+}
+
+#[test]
+fn test3() {
+    let nums = vec![-1];
+    assert_eq!(Solution::sorted_squares(nums), [1]);
 }
