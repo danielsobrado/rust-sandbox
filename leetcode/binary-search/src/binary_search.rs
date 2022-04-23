62c67457e1097c966a2b5751ba0f445cd935e5d2
danielsobrado <daniel@danielsobrado.com>
2022-04-23 13:06:41 +0400

    Leetcode: Solution for binary-search

--- leetcode/binary-search/src/binary_search.rs
+++ leetcode/binary-search/src/binary_search.rs
@@ -6,7 +6,33 @@ struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        0
+
+        if nums.len() == 0 {
+            return -1;
+        } else if nums.len() == 1 {
+            if nums[0] == target {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            if nums[mid] == target {
+                return mid as i32;
+            } else if nums[mid] < target {
+                left = mid + 1;
+            } else {
+                if mid == 0 {
+                    return -1;
+                }
+                right = mid - 1;
+            }
+        }
+        -1
     }
 }
 
@@ -18,4 +44,9 @@ fn test1() {
 #[test]
 fn test2() {
     assert_eq!(Solution::search(vec![-1,0,3,5,9,12], 2), -1);
+}
+
+#[test]
+fn test3() {
+    assert_eq!(Solution::search(vec![2,5], 0), -1);
 }
