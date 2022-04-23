66713e40a78d9df18af0641ce45ade038469abdc
danielsobrado <daniel@danielsobrado.com>
2022-04-23 10:18:47 +0400

    Leetcode: Solution for reverse-words-in-a-string-iii

--- leetcode/reverse-words-in-a-string-iii/src/reverse_words_in_a_string_iii.rs
+++ leetcode/reverse-words-in-a-string-iii/src/reverse_words_in_a_string_iii.rs
@@ -3,10 +3,32 @@ struct Solution;
 // Problem URL: https://leetcode.com/problems/reverse-words-in-a-string-iii/
 // Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 
-
 impl Solution {
     pub fn reverse_words(s: String) -> String {
-        
+
+        if s.len() <= 1 {
+            return s;
+        }
+
+        let mut s = s;
+        let mut result = String::new();
+
+        let mut end = s.find(' ').unwrap_or(s.len());
+
+        loop {
+            let word = &s[0..end].chars().rev().collect::<String>();
+            result = result + word;
+            
+            if end != s.len() {
+                s = s[end+1..].to_string();
+                end = s.find(' ').unwrap_or(s.len());
+                result.push(' ');
+            } else {
+                break;
+            }
+        }
+
+        result
     }
 }
 
@@ -18,4 +40,9 @@ fn test1() {
 #[test]
 fn test2() {
     assert_eq!(Solution::reverse_words(String::from("God Ding")), String::from("doG gniD"));
+}
+
+#[test]
+fn test3() {
+    assert_eq!(Solution::reverse_words(String::from("Single")), String::from("elgniS"));
 }
