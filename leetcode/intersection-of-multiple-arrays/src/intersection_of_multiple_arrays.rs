2e7afcfef354a36a031c996719574057191aa751
danielsobrado <daniel@danielsobrado.com>
2022-04-24 08:00:03 +0400

    Leetcode: solve intersection-of-multiple-arrays

--- leetcode/intersection-of-multiple-arrays/src/intersection_of_multiple_arrays.rs
+++ leetcode/intersection-of-multiple-arrays/src/intersection_of_multiple_arrays.rs
@@ -4,9 +4,37 @@ struct Solution;
 // Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers, 
 // return the list of integers that are present in each array of nums sorted in ascending order.
 
+use std::collections::HashMap;
+
 impl Solution {
     pub fn intersection(nums: Vec<Vec<i32>>) -> Vec<i32> {
+
+        let l = nums.len() as i32;
+
+        if l == 0 {
+            return vec![];
+        } else if l == 1 {
+            let result = nums[0].clone().sort();
+            result
+        }
+
+        let mut nums_map:HashMap<i32, i32> = HashMap::new();
+
+        for nums_slice in nums {
+            for num in nums_slice {
+                // Count no. of occurences of each number in hashmap
+                *nums_map.entry(num).or_insert(0) += 1;
+            }
+        }
+
+        let mut result = nums_map.into_iter()
+                                    .filter(|&(_, v)|v == l)
+                                    .map(|(i, _v)|{i})
+                                    .collect::<Vec<i32>>();
         
+        result.sort();
+        result
+
     }
 }
 
